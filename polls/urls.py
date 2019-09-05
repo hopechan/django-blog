aea2a66a7cb447bd474547a8c8c80f110cebed27
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-05 16:19:47 -0600

    se agregaron las vistas
    polls/detalle
    polls/resultados
    polls/voto

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:question_id>/', views.detalle, name='detalle'),
+    path('<int:question_id>/resultados', views.resultados, name='resultados'),
+    path('<int:question_id/voto>', views.voto, name="voto")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
 
-def index(reques):
+def index(request):
     return HttpResponse("Hola mundo desde la vista polls")
+
+def detalle(request, question_id):
+    return HttpResponse("Estas viendo la pregunta %s. " % question_id)
+
+def resultados(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s. "
+    return HttpResponse(response % question_id)
+
+def voto(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s. " % question_id)
