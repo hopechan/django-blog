681da491a2aab4e00d99551354b9b31713e65ee7
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 08:47:53 -0600

    muestra las primeras 5 preguntas separadas por coma

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hola mundo desde la vista polls")
+    #con [:5] toma las primeras 5 preguntas y en orden descendiente segun su fecha de publicacion
+    preguntas_recientes = Question.objects.order_by('-pub_date')[:5]
+    salida = ', '.join([q.question_text for q in preguntas_recientes])
+    return HttpResponse(salida)
 
 def detalle(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s. " % question_id)
