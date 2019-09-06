291ebb2355f3355d502c3365368d428d946871ed
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 11:47:29 -0600

    la url para detalle a cambiado
    ahora es /polls/detalle/id

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:question_id>/', views.detalle, name='detalle'),
+    path('detalle/<int:question_id>/', views.detalle, name='detalle'),
     path('<int:question_id>/resultados', views.resultados, name='resultados'),
     path('<int:question_id/voto>', views.voto, name="voto")
 ]
