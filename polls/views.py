13f54af3cba90d1ef1876633aa49093f7abdf9a9
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 09:04:03 -0600

    vista principal creada

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 def index(request):
     #con [:5] toma las primeras 5 preguntas y en orden descendiente segun su fecha de publicacion
     preguntas_recientes = Question.objects.order_by('-pub_date')[:5]
-    salida = ', '.join([q.question_text for q in preguntas_recientes])
-    return HttpResponse(salida)
+    #el preguntas recientes dentro de las comillas simples es la variable que se usa en el html
+    context = {
+        'preguntas_recientes' : preguntas_recientes
+    }
+    return render(request, 'polls/index.html', context)
 
 def detalle(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s. " % question_id)
