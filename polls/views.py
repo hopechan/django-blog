d32a558540da782dc1752cc22997d452222bd334
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 10:54:40 -0600

    Manejo de errores 404
    - Se elimino import para loader
    - Se creo vista detalle

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -13,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detalle(request, question_id):
-    return HttpResponse("Estas viendo la pregunta %s. " % question_id)
+    try:
+        pregunta = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("La pregunta no existe :c")
+    return render(request, 'polls/detalle.html', {'pregunta' : pregunta})
 
 def resultados(request, question_id):
     response = "Estas viendo los resultados de la pregunta %s. "
