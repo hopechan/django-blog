ff8247816da184650369f6f519fa0a72e93a9f6b
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 11:35:54 -0600

    manejo de error 404 mejorada

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -12,11 +12,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detalle(request, question_id):
-    try:
-        pregunta = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("La pregunta no existe :c")
-    return render(request, 'polls/detalle.html', {'pregunta' : pregunta})
+    pregunta = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detalle.html', { 'pregunta' : pregunta })
 
 def resultados(request, question_id):
     response = "Estas viendo los resultados de la pregunta %s. "
