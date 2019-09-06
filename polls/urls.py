360c1a3dba7a4d1d4bfaa472d4781dc3a2c4cbf5
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-06 11:50:05 -0600

    las vistas ahora se identifican por su app correspondiente

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#nombre de la app
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name="index"),
     path('detalle/<int:question_id>/', views.detalle, name='detalle'),
