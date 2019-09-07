a2b195a39e7fcff517a87b0816bd0209d8784e17
Esperanza Duenas <da13002@ues.edu.sv>
2019-09-07 15:17:33 -0600

    pruebas a metodo publicada_recientemente terminadas

--- polls/tests.py
+++ polls/tests.py
@@ -11,3 +11,13 @@ class QuestionModelTest(TestCase):
         pregunta = Question(pub_date=fecha_adelantada)
         self.assertIs(pregunta.publicada_recientemente(), False)
 
+    def test_se_publico_en_el_pasado(self):
+        #a la fecha actual se le resta 1 dia con 1 segundo
+        fecha_atrasada = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        pregunta_antigua = Question(pub_date=fecha_atrasada)
+        self.assertIs(pregunta_antigua.publicada_recientemente(), False)
+
+    def test_se_publico_recientemente(self):
+        fecha_reciente = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        pregunta_reciente = Question(pub_date=fecha_reciente)
+        self.assertIs(pregunta_reciente.publicada_recientemente(), True)
