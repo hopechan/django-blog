1ddc08392060322c634f783628742525a17e1ca8
Esperanza Duenas <da13002@ues.edu.sv>
2019-09-07 11:43:41 -0600

    la prueba falla

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTest(TestCase):
+    def test_se_publico_con_fecha_en_el_futuro(self):
+        #se coloca una fecha adelantada 30 dias a la actual
+        fecha_adelantada = timezone.now() + datetime.timedelta(days=30)
+        #se le asigna la fecha adelantada a la propiedad pub_date de un objeto Question
+        pregunta = Question(pub_date=fecha_adelantada)
+        self.assertIs(pregunta.publicada_recientemente(), False)
+
