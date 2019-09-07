4e09063daaa9a71d7173d5bc85dcce13933bc9ed
Esperanza Duenas <da13002@ues.edu.sv>
2019-09-06 21:14:54 -0600

    correcion de url antes era /detalle/polls/id ahora es /polls/id

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name="index"),
-    path('detalle/<int:question_id>/', views.detalle, name='detalle'),
+    path('<int:question_id>/', views.detalle, name='detalle'),
     path('<int:question_id>/resultados', views.resultados, name='resultados'),
     path('<int:question_id>/vote/', views.voto, name='voto'),
 ]
