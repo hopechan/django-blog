588f3c71982c355a475a8fe47f23731b4907d0e5
Esperanza Dueñas <da13002@ues.edu.sv>
2019-09-11 10:53:57 -0600

    english translation

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('<int:pk>/', views.DetalleView.as_view(), name='detalle'),
-    path('<int:pk>/resultados/', views.ResultadosView.as_view(), name='resultados'),
-    path('<int:question_id>/voto/', views.voto, name='voto'),
+    path('<int:pk>/', views.DetalleView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/voto/', views.vote, name='vote'),
 ]
